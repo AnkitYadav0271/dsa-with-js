876a04949187c2ca136e747c21946b1f87687f8a
Ankit Yadav <ankitofficial0271@gmail.com>
2025-10-14 00:12:07 +0530

    optimized code

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,16 +1,18 @@
-let arr = [12,34,45,67,8,99,0,34,4,56,7,8,9,23];
+let arr = [12, 34, 45, 67, 8, 99, 0, 34, 4, 56, 7, 8, 9, 23];
 
-function bubbleSort(arr){
-    for (let i = 0; i < arr.length -  1; i++){
-        for (let j = 0; j < arr.length - i - 1; j++){
-        if(arr[j] > arr[j+1]){
-            let temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
-        }  
-        }
-        }
-
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let isSwapped = false;
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        isSwapped = true;
+      }
+    }
+    if (!isSwapped) break; //it check if no swapping is going on then break the loop
+  }
 }
 bubbleSort(arr);
 console.log(arr);
